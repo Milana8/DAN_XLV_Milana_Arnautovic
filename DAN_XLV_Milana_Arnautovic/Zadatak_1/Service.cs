3153f433eb3c74285fa4551c551ece2f9f907823
Milana8 <91amilana@gmail.com>
2020-07-13 17:36:55 +0200

    Add LoginView, LoginViewModel, Service

---
+++ DAN_XLV_Milana_Arnautovic/Zadatak_1/Service.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zadatak_1.Model;
+
+namespace Zadatak_1
+{
+   public class Service
+    {
+        public static List<tblProduct> products = new List<tblProduct>();
+        public static tblProduct currentProduct = new tblProduct();
+
+        public List<tblProduct> GetAllProducts()
+        {
+            try
+            {
+                using (DAN_XLVEntities context = new DAN_XLVEntities())
+                {
+                    List<tblProduct> products = new List<tblProduct>();
+                    products = context.tblProducts.ToList();
+                    return products;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message.ToString());
+                return null;
+            }
+        }
+
+        public void DeleteProduct(int ProductID)
+        {
+            try
+            {
+                using (DAN_XLVEntities context = new DAN_XLVEntities())
+                {
+                    tblProduct productToDelete = (from r in context.tblProducts where r.ID == ProductID select r).First();
+                    context.tblProducts.Remove(productToDelete);
+                    context.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message.ToString());
+            }
+        }
+
+
+    }
+}
---
+++ DAN_XLV_Milana_Arnautovic/Zadatak_1/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using Zadatak_1.Command;
+using Zadatak_1.Model;
+using Zadatak_1.View;
+
+namespace Zadatak_1.ViewModel
+{
+    class MainWindowViewModel: ViewModelBase
+    {
+              
+    }
+}
+    
+
